0bca43102c5695fe14021510a9d216aaec722f6d
Drew Parsons <dparsons@debian.org>
2006-10-10 13:33:43 +0000

    New upstream versions (X11R7.1): xf86-input-elo2300-1.1.0 xf86-input-elographics-1.1.0 xf86-input-fpit-1.1.0 xf86-input-jamstudio-1.1.0 xf86-input-joystick-1.1.0

--- src/xf86Elo.c
+++ src/xf86Elo.c
@@ -57,15 +57,13 @@
 
 #ifdef XFREE86_V4
 
-#ifndef XFree86LOADER
 #include <unistd.h>
 #include <errno.h>
-#endif
+#include <string.h>
 
 #include "misc.h"
 #include "xf86.h"
 #if !defined(DGUX)
-#include "xf86_ansic.h"
 #endif
 #include "xf86_OSproc.h"
 #include "xf86Xinput.h"
@@ -1825,7 +1823,7 @@ static XF86ModuleVersionInfo version_rec = {
   MODINFOSTRING1,
   MODINFOSTRING2,
   XORG_VERSION_CURRENT,
-  1, 0, 0,
+  1, 1, 0,
   ABI_CLASS_XINPUT,
   ABI_XINPUT_VERSION,
   MOD_CLASS_XINPUT,
